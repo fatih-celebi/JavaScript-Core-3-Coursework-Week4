ca866370f2ec9c7a5f6a9fe83c4bc9415e63911b
fatih-celebi <celebifatih.c1@gmail.com>
2022-11-23 23:47:08 +0000

    Complete Task A-02

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,20 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+    let onlyNumbers = numbers.filter((item) => {
+        return typeof item == "number";
+    });
+    let sum = 0;
+    onlyNumbers.forEach(number => {
+        sum += number;
+    });
+    let average = sum / onlyNumbers.length;
+    return average;
+}
+
+//numbers = [1, 2, 6, 8, "aaa", true, 76];
+//console.log(average(numbers));
+
 
 module.exports = average;
